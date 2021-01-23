c806cafcc5a9cf2deed16406035510b6c436c2a2
OHMAE Ryosuke <ryo@mm2d.net>
2021-01-23 16:21:24 +0900

    🆙gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.2")
         classpath(kotlin("gradle-plugin", version = "1.4.21"))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2")
 
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.29.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
     }
 }
 
