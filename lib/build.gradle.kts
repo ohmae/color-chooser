523c5d22fe0e789fd3b8004317345b0cdb8ac925
OHMAE Ryosuke <ryo@mm2d.net>
2021-06-05 11:12:43 +0900

    🆙kotlin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -44,9 +44,9 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.core:core-ktx:1.5.0")
     implementation("com.google.android.material:material:1.3.0")
     testImplementation("junit:junit:4.13.2")
 }
