19dfabb379fc8e1c578726462268410252248f61
OHMAE Ryosuke <ryo@mm2d.net>
2022-01-12 00:04:13 +0900

    ♻delete deprecated method

--- lib/src/main/java/net/mm2d/color/chooser/ColorChooserDialog.kt
+++ lib/src/main/java/net/mm2d/color/chooser/ColorChooserDialog.kt
@@ -7,9 +7,7 @@
 
 package net.mm2d.color.chooser
 
-import android.app.Activity
 import android.app.Dialog
-import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -27,7 +25,6 @@ import net.mm2d.color.chooser.databinding.Mm2dCcColorChooserBinding
  * Color chooser dialog
  */
 object ColorChooserDialog {
-    private const val KEY_REQUEST_CODE = "KEY_REQUEST_CODE"
     private const val KEY_REQUEST_KEY = "KEY_REQUEST_KEY"
     private const val KEY_INITIAL_COLOR = "KEY_INITIAL_COLOR"
     private const val KEY_WITH_ALPHA = "KEY_WITH_ALPHA"
@@ -88,52 +85,6 @@ object ColorChooserDialog {
     fun registerListener(
         fragment: Fragment,
         requestKey: String,
-        listener: ColorChooserListener
-    ) {
-        registerListener(
-            fragment.childFragmentManager,
-            requestKey,
-            fragment.viewLifecycleOwner,
-            listener
-        )
-    }
-
-    /**
-     * Register result listener.
-     *
-     * Call at the timing of onCreate of activity.
-     *
-     * @param requestKey Request Key, pass the same value to the `show`
-     * @param activity Caller fragment activity
-     * @param listener Listener receiving the result
-     */
-    @Deprecated("should use registerListener(FragmentActivity, String, ColorChooserListener)")
-    fun registerListener(
-        requestKey: String,
-        activity: FragmentActivity,
-        listener: ColorChooserListener
-    ) {
-        registerListener(
-            activity.supportFragmentManager,
-            requestKey,
-            activity,
-            listener
-        )
-    }
-
-    /**
-     * Register result listener.
-     *
-     * Call at the timing of onViewCreated of fragment.
-     *
-     * @param requestKey Request Key, pass the same value to the `show`
-     * @param fragment Caller fragment
-     * @param listener Listener receiving the result
-     */
-    @Deprecated("should use registerListener(Fragment, String, ColorChooserListener)")
-    fun registerListener(
-        requestKey: String,
-        fragment: Fragment,
         listener: ColorChooserListener
     ) {
         registerListener(
@@ -209,87 +160,6 @@ object ColorChooserDialog {
         )
     }
 
-
-    /**
-     * Result callback implements to Fragment or Activity.
-     *
-     * This is deprecated and will be removed in a future version.
-     * Please use ColorChooserListener and register by registerListener instead.
-     */
-    @Deprecated("should use registerListener")
-    interface Callback {
-        /**
-         * Call at close dialog
-         *
-         * @param requestCode requestCode of show parameter
-         * @param resultCode `Activity.RESULT_OK` or `Activity.RESULT_CANCELED`
-         * @param color selected color
-         */
-        fun onColorChooserResult(requestCode: Int, resultCode: Int, color: Int)
-    }
-
-    /**
-     * Show dialog.
-     *
-     * This is deprecated and will be removed in a future version.
-     * Please use show(FragmentActivity, String, Int, Boolean, Int) instead.
-     *
-     * @param activity FragmentActivity
-     * @param requestCode use in listener call
-     * @param initialColor initial color
-     * @param withAlpha if true, alpha section is enabled
-     * @param initialTab initial tab, TAB_PALETTE/TAB_HSV/TAB_RGB
-     */
-    @Deprecated("Please use show(FragmentActivity, String, Int, Boolean, Int) instead")
-    fun show(
-        activity: FragmentActivity,
-        requestCode: Int = 0,
-        initialColor: Int = Color.WHITE,
-        withAlpha: Boolean = false,
-        initialTab: Int = TAB_PALETTE
-    ) {
-        show(
-            activity.supportFragmentManager,
-            bundleOf(
-                KEY_REQUEST_CODE to requestCode,
-                KEY_INITIAL_COLOR to initialColor,
-                KEY_WITH_ALPHA to withAlpha,
-                KEY_INITIAL_TAB to initialTab,
-            )
-        )
-    }
-
-    /**
-     * Show dialog.
-     *
-     * This is deprecated and will be removed in a future version.
-     * Please use show(FragmentActivity, String, Int, Boolean, Int) instead.
-     *
-     * @param fragment Fragment
-     * @param requestCode use in listener call
-     * @param initialColor initial color
-     * @param withAlpha if true, alpha section is enabled
-     * @param initialTab initial tab, TAB_PALETTE/TAB_HSV/TAB_RGB
-     */
-    @Deprecated("should use show(Fragment, String, Int, Boolean, Int)")
-    fun show(
-        fragment: Fragment,
-        requestCode: Int = 0,
-        initialColor: Int = Color.WHITE,
-        withAlpha: Boolean = false,
-        initialTab: Int = TAB_PALETTE
-    ) {
-        show(
-            fragment.childFragmentManager,
-            bundleOf(
-                KEY_REQUEST_CODE to requestCode,
-                KEY_INITIAL_COLOR to initialColor,
-                KEY_WITH_ALPHA to withAlpha,
-                KEY_INITIAL_TAB to initialTab,
-            )
-        )
-    }
-
     private fun show(manager: FragmentManager, arguments: Bundle) {
         if (manager.findFragmentByTag(TAG) != null) return
         if (manager.isStateSaved) return
@@ -343,29 +213,11 @@ object ColorChooserDialog {
             outState.putInt(KEY_INITIAL_COLOR, colorChooserView.color)
         }
 
-        override fun onCancel(dialog: DialogInterface) {
-            val requestCode = requireArguments().getInt(KEY_REQUEST_CODE)
-            extractCallback()?.onColorChooserResult(requestCode, Activity.RESULT_CANCELED, 0)
-        }
-
         private fun notifySelect() {
-            val color = colorChooserView.color
-            val arguments = requireArguments()
-            val key = arguments.getString(KEY_REQUEST_KEY)
-            if (key != null) {
-                parentFragmentManager.setFragmentResult(key, bundleOf(RESULT_KEY_COLOR to color))
-            } else {
-                val requestCode = arguments.getInt(KEY_REQUEST_CODE)
-                extractCallback()?.onColorChooserResult(
-                    requestCode,
-                    Activity.RESULT_OK,
-                    color
-                )
-            }
-        }
-
-        private fun extractCallback(): Callback? {
-            return parentFragment as? Callback ?: activity as? Callback
+            val key = requireArguments().getString(KEY_REQUEST_KEY) ?: return
+            parentFragmentManager.setFragmentResult(
+                key, bundleOf(RESULT_KEY_COLOR to colorChooserView.color)
+            )
         }
     }
 }
