f352abfdd25dfa6206ce81fff386ec3feec46e25
OHMAE Ryosuke <ryo@mm2d.net>
2022-07-22 23:39:01 +0900

    🆙dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath(kotlin("gradle-plugin", version = "1.7.10"))
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0")
 
         classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.android.tools.build:gradle:7.2.1")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
+    implementation("org.jetbrains.dokka:dokka-core:1.7.10")
 }
