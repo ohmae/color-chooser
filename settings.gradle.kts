c6c515246f1d951d394d3404cc2c4030fe7f2b33
OHMAE Ryosuke <ryo@mm2d.net>
2023-11-21 22:38:12 +0900

    🆙AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
     }
 }
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
 }
 rootProject.name = "color-chooser"
 include(":lib")
