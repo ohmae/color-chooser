abb53a69c75b04f5155693f2f613e6a9111f4886
OHMAE Ryosuke <ryo@mm2d.net>
2024-03-23 13:34:17 +0900

    🎉release 0.7.3

--- build-logic/src/main/kotlin/net/mm2d/build/Projects.kt
+++ build-logic/src/main/kotlin/net/mm2d/build/Projects.kt
@@ -19,7 +19,7 @@ object Projects {
 
     private const val versionMajor: Int = 0
     private const val versionMinor: Int = 7
-    private const val versionPatch: Int = 2
+    private const val versionPatch: Int = 3
     const val versionName: String = "$versionMajor.$versionMinor.$versionPatch"
 
     object Url {
