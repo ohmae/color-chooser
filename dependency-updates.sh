eb871b406879b51ee9644f0e7e5323235b77583e
OHMAE Ryosuke <ryo@mm2d.net>
2025-10-26 00:18:06 +0900

    🆙kotlin

--- dependency-updates.sh
+++ dependency-updates.sh
@@ -2,5 +2,5 @@
 if [ -z "$1" ]; then
     ./gradlew dependencyUpdates --no-configuration-cache --no-parallel
 else
-    ./gradlew :${1}:dependencyUpdates --no-configuration-cache --no-parallel
+    ./gradlew :"${1}":dependencyUpdates --no-configuration-cache --no-parallel
 fi
