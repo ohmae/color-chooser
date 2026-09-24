24a15eef7e35d6cc4dcd21ea5e9b98f99f604407
OHMAE Ryosuke <ryo@mm2d.net>
2026-09-24 15:40:10 +0900

    fix preview padding

--- chooser-compose/src/main/kotlin/net/mm2d/color/chooser/compose/ColorChooserScreen.kt
+++ chooser-compose/src/main/kotlin/net/mm2d/color/chooser/compose/ColorChooserScreen.kt
@@ -7,21 +7,14 @@
 
 package net.mm2d.color.chooser.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -33,9 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -44,12 +35,8 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.mm2d.color.chooser.compose.util.ColorSaver
-import net.mm2d.color.chooser.compose.util.alphaBackgroundBrush
 import net.mm2d.color.chooser.compose.util.to8bitInt
 import net.mm2d.color.chooser.compose.util.toChooserColor
-
-private val PreviewShape = RoundedCornerShape(24.dp)
-private val PreviewLabelShape = RoundedCornerShape(percent = 50)
 
 @Immutable
 class ColorChooserColors internal constructor(
@@ -404,75 +391,6 @@ internal fun ColorChooserContent(
         }
     }
 }
-
-@Composable
-private fun ColorPreview(
-    initialColor: Color,
-    resultColor: Color,
-    withAlpha: Boolean,
-    labelColor: Color,
-    labelBackgroundColor: Color,
-    labelTextStyle: TextStyle,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .clip(PreviewShape),
-    ) {
-        Surface(
-            modifier = Modifier
-                .weight(1f)
-                .background(alphaBackgroundBrush()),
-            color = initialColor,
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                Text(
-                    text = initialColor.toHex(withAlpha),
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .background(labelBackgroundColor, PreviewLabelShape)
-                        .padding(horizontal = 10.dp, vertical = 4.dp)
-                        .align(Alignment.BottomStart),
-                    style = labelTextStyle,
-                    color = labelColor,
-                )
-            }
-        }
-        Surface(
-            modifier = Modifier
-                .weight(2f)
-                .background(alphaBackgroundBrush()),
-            color = resultColor,
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                Text(
-                    text = resultColor.toHex(withAlpha),
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .background(labelBackgroundColor, PreviewLabelShape)
-                        .padding(horizontal = 10.dp, vertical = 4.dp)
-                        .align(Alignment.BottomEnd),
-                    style = labelTextStyle,
-                    color = labelColor,
-                )
-            }
-        }
-    }
-}
-
-private fun Color.toHex(
-    withAlpha: Boolean,
-): String =
-    if (withAlpha) {
-        "#%08X".format(toArgb())
-    } else {
-        "#%06X".format(toArgb() and 0xFFFFFF)
-    }
 
 @PreviewLightDark
 @Composable
---
+++ chooser-compose/src/main/kotlin/net/mm2d/color/chooser/compose/ColorPreview.kt
@@ -0,0 +1,92 @@
+package net.mm2d.color.chooser.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import net.mm2d.color.chooser.compose.util.alphaBackgroundBrush
+
+private val PreviewShape = RoundedCornerShape(24.dp)
+private val PreviewLabelShape = RoundedCornerShape(percent = 50)
+
+@Composable
+internal fun ColorPreview(
+    initialColor: Color,
+    resultColor: Color,
+    withAlpha: Boolean,
+    labelColor: Color,
+    labelBackgroundColor: Color,
+    labelTextStyle: TextStyle,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(PreviewShape),
+    ) {
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .background(alphaBackgroundBrush()),
+            color = initialColor,
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Text(
+                    text = initialColor.toHex(withAlpha),
+                    modifier = Modifier
+                        .padding(start = 16.dp, bottom = 8.dp)
+                        .background(labelBackgroundColor, PreviewLabelShape)
+                        .padding(horizontal = 10.dp, vertical = 4.dp)
+                        .align(Alignment.BottomStart),
+                    style = labelTextStyle,
+                    color = labelColor,
+                )
+            }
+        }
+        Surface(
+            modifier = Modifier
+                .weight(2f)
+                .background(alphaBackgroundBrush()),
+            color = resultColor,
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Text(
+                    text = resultColor.toHex(withAlpha),
+                    modifier = Modifier
+                        .padding(end = 16.dp, bottom = 8.dp)
+                        .background(labelBackgroundColor, PreviewLabelShape)
+                        .padding(horizontal = 10.dp, vertical = 4.dp)
+                        .align(Alignment.BottomEnd),
+                    style = labelTextStyle,
+                    color = labelColor,
+                )
+            }
+        }
+    }
+}
+
+private fun Color.toHex(
+    withAlpha: Boolean,
+): String =
+    if (withAlpha) {
+        "#%08X".format(toArgb())
+    } else {
+        "#%06X".format(toArgb() and 0xFFFFFF)
+    }
