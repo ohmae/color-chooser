b1948a51d8a5b4b13b80f4c071be3a1144698e6b
OHMAE Ryosuke <ryo@mm2d.net>
2026-09-25 23:42:42 +0900

    release compose version 0.2.1

--- build-logic/src/main/kotlin/net/mm2d/build/Projects.kt
+++ build-logic/src/main/kotlin/net/mm2d/build/Projects.kt
@@ -27,7 +27,7 @@ object Projects {
     object Compose {
         private const val VERSION_MAJOR: Int = 0
         private const val VERSION_MINOR: Int = 2
-        private const val VERSION_PATCH: Int = 0
+        private const val VERSION_PATCH: Int = 1
         const val VERSION_NAME: String = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
     }
 
